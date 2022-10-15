e1fe6a1ec7415e4f22dacaa41a8d5056529f4380
Vinanddrinks <66210430+Vinanddrinks@users.noreply.github.com>
2022-10-15 19:44:31 -0400

    setted up api for quizz 2.0     added get method for drinks     added get method for drinks by id     added get method for drinks by name     added get method for drinks by popularity

---
+++ API/routes/drinks.js
@@ -0,0 +1,48 @@
+var express = require("express");
+var router = express.Router();
+var drinks = JSON.parse(require("fs").readFileSync("mokData/coffees.json"));
+
+// GET drinks listing.
+router.get("/", function (req, res) {
+  res.json(drinks);
+});
+
+// GET Hot drinks listing.
+router.get("/hot", function (req, res) {
+  res.json(drinks.filter((drink) => drink.is_hot));
+});
+
+// GET Cold drinks listing.
+router.get("/cold", function (req, res) {
+  res.json(drinks.filter((drink) => !drink.is_hot));
+});
+module.exports = router;
+
+// GET best drinks listing.
+router.get("/best", function (req, res) {
+  res.json(
+    drinks
+      .filter((drink) => drink.average_rating >= 4)
+      .sort((a, b) => b.average_rating - a.average_rating)
+  );
+});
+// GET popular drinks listing.
+router.get("/popular", function (req, res) {
+  res.json(
+    drinks
+      .filter((drink) => drink.average_rating >= 3.5)
+      .sort((a, b) => b.count_orders - a.count_orders)
+  );
+});
+// GET drink by id.
+router.get("/:id", function (req, res) {
+  res.json(drinks.find((drink) => drink.id === req.params.id));
+});
+// GET drink by name.
+router.get("/name/:name", function (req, res) {
+  res.json(
+    drinks.filter((drink) =>
+      drink.name.toLowerCase().includes(req.params.name.toLowerCase())
+    )
+  );
+});

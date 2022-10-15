4a2861605849621bf509f30dc757cecfa3ac7cd4
Vinanddrinks <66210430+Vinanddrinks@users.noreply.github.com>
2022-10-15 16:29:34 -0400

    added api project and search view

---
+++ src/views/searchView.vue
@@ -0,0 +1,22 @@
+<template>
+    <div id="globalContainer">
+        
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped lang="scss">
+#globalContainer {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    height: 100%;
+}
+</style>
